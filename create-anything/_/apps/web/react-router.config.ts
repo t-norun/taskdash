413b08d937120cd71abbad6b95a3887bd2d58387
Unions <engineer.joint102@gmail.com>
2026-03-06 23:35:08 +0900

    Fix React Router prerender config for production build

--- create-anything/_/apps/web/react-router.config.ts
+++ create-anything/_/apps/web/react-router.config.ts
@@ -1,7 +1,6 @@
-import type { Config } from '@react-router/dev/config';
+import type { Config } from "@react-router/dev/config";
 
 export default {
-	appDirectory: './src/app',
-	ssr: true,
-	prerender: ['/*?'],
+  appDirectory: "./src/app",
+  ssr: true,
 } satisfies Config;
